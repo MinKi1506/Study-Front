5ea83fc50e2805a9718ee915a472f2d6c0690fff
MinKi1506 <93629619+MinKi1506@users.noreply.github.com>
2022-07-25 17:46:34 +0900

    Update safe.sol

--- 7.19KAS/contracts/safe.sol
+++ 7.19KAS/contracts/safe.sol
@@ -11,9 +11,9 @@ contract safe{
         string date; //점검 시간
     }
 
-    mapping(string => check_list) internal lists;
+    mapping(string => check_list) internal lists; // lists에서 string을 키값으로 check_list를 맵핑한다(찾는다)
 
-    check_list[] internal checks; //구조체를 배열로 선언
+    check_list[] internal checks; //구조체를 배열로 선언ㅇ
 
     function add_check(address _checker, string memory _num, string memory _result, string memory _date) public {
         count++;
@@ -24,15 +24,17 @@ contract safe{
         lists[_num].date = _date;
     }
 
+    //총 리뷰의 갯수 조회하기
     function total_count() public view returns(uint32){
         return count;
     }
 
-    function get_check(uint _index) public view returns (address, string memory, string memory, string memory){
+    //해당 리뷰 인덱스의 점검 상세 내용 조회하기
+    function get_checks(uint _index) public view returns (address, string memory, string memory, string memory){
         return (checks[_index].checker, checks[_index].num, checks[_index].result, checks[_index].date);
     }
 
-    
+    //해당 리뷰 번호의 점검 리스트 조회하기
     function get_list(string memory _num) public view returns (address, string memory, string memory){
         return (lists[_num].checker, lists[_num].result, lists[_num].date); 
     }
